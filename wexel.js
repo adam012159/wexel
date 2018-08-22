eecd75aa242f111df6f19242dc8bd074fdb1109d
Adam Jarry <adamjarry@gmail.com>
2018-08-22 17:13:41 -0400

    Added the ability to mute and unmute a user, and fixed the logging system
    -You can mute a user by typing 'muteuser (seconds) @mention'
    -you can unmute a user by typing 'unmuteuser @mention'
    -Muting is server-wide, so you wont be affecting them in another Wexel server
    -Users who are muted will get a pm

--- wexel.js
+++ wexel.js
@@ -6,6 +6,8 @@ const config = require("./config.json");
 const version = require("./version.json");
 const talkedRecently = new Set();
 const talkreseter = new Set();
+const muted = new Set();
+const mutedreseter = new Set();
 var errorlog = " ";
 var outputlog = " ";
 var hours = 0;
@@ -18,13 +20,18 @@ function removefromtalkedrecently(name) {
     talkedRecently.delete(name);
     return;
 }
+function unmuteUser(entry, name) {
+    muted.delete(entry);
+    console.log(colors.cyan(`${name} is no longer muted.`));
+    return;
+}
 function writeJSON(a, b, path) {
     a = JSON.stringify(a);
     fs.writeFileSync(`${path}/${b}.json`, a);
     return;
 }
 function writeLog(log, fileName, path) {
-    fs.writeFileSync(`${path}/${b}.log`, a);
+    fs.writeFileSync(`${path}/${fileName}.log`, log);
     return;
 }
 function readstat(a, path) {
@@ -91,19 +98,21 @@ function updateserver(oldfile, server) {
 }
 function runEveryDay() {
     hours = hours + 1;
-    if (hours === 24) {
+    if (hours >= 24) {
         let now = new Date();
         writeLog(errorlog, `Errors ${months[now.getMonth()]}/${now.getDate()}`, "logs");
         writeLog(outputlog, `Output ${months[now.getMonth()]}/${now.getDate()}`, "logs");
+        outputlog = " ";
+        errorLog = " ";
         hours = 0;
     }
+    return;
 }
 
 
 client.on("error", (e) => {
     let now = new Date();
     console.error(colors.red(`${e}`));
-    client.channels.get("456482534482509835").send("An error has occured. Please check log for more information.");
     errorlog = errorlog + `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} ${e} \n`;
 });
 client.on("warn", (e) => console.warn(colors.yellow(`${e}`)));
@@ -189,7 +198,55 @@ client.on("message", (message) => {
                 writeJSON(settings, server, "servers");
                 return;
             }
+            if (message.content.includes("unmuteuser")) {
+                let targetUser = message.mentions.members.first();
+                if (targetUser === undefined) {
+                    message.channel.send(`You need to specify a user to unmute. Correct syntax is '${settings.prefix}unmuteuser @mention`);
+                    return;
+                }
+                if (muted.has(`${targetUser.id}${message.guild}`) === false) {
+                    message.channel.send(`${targetUser.tag} is not muted.`);
+                    return;
+                }
+                targetUser.send(`You have been unmuted in ${message.guild}`)
+                    .catch(console.error);
+                message.delete(50);
+                console.log(colors.cyan(`${message.author.tag} in ${message.guild} unmuted ${targetUser.tag}...`));
+                outputLog = outputlog + `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} ${message.author.tag} in ${message.guild} unmuted ${targetUser.tag}... \n`;
+                muted.delete(`${targetUser.id}${message.guild}`);
+                let mutedUsersGuild = targetUser.id + message.guild.id;
+                clearTimeout(mutedreseter[mutedUsersGuild]);
+                return;
+            }
+            if (message.content.includes("muteuser")) {
+                let targetUser = message.mentions.members.first();
+                if (targetUser === undefined) {
+                    message.channel.send(`You need to specify a user to mute. Correct syntax is '${settings.prefix}muteuser (seconds to mute) @mention`);
+                    return;
+                }
+                let strArray = message.content.split(" ");
+                strArray = strArray[1];
+                if (isNaN(strArray)) {
+                    message.channel.send(`${strArray} is not a number. Correct syntax is '${settings.prefix}muteuser (seconds to mute) @mention`);
+                    return;
+                }
+                targetUser.send(`You have been muted in ${message.guild} for ${strArray} seconds. If you have any questions on why you were muted, please contact the server's owner.`)
+                    .catch(console.error);
+                message.delete(50);
+                console.log(colors.cyan(`${message.author.tag} in ${message.guild} muted ${targetUser.tag} for ${strArray} seconds...`));
+                outputLog = outputlog + `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} ${message.author.tag} in ${message.guild} muted ${targetUser.tag} for ${strArray} seconds... \n`;
+                strArray = strArray * 1000;
+                muted.add(`${targetUser.id}${message.guild}`);
+                let mutedUsersGuild = targetUser.id + message.guild.id;
+                mutedreseter[mutedUsersGuild] = setTimeout(unmuteUser, strArray, `${targetUser.id}${message.guild}`, targetUser.tag);
+                return;
+            }
         }
+    }
+    if (muted.has(`${message.author.id}${message.guild}`)) {
+        message.delete(50);
+        console.log(colors.cyan(`Deleted message from ${message.author.tag} due to mute...`));
+        outputlog = outputlog + `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} Deleted message from ${message.author.tag} due to mute... \n`;
     }
     if (talkedRecently.has(message.author.id)) {
         message.delete(50);
